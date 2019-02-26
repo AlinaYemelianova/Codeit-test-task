e4486b90e65cea341926d24b32b3e1cc9647128f
Alina <alinayemelianova8888@gmail.com>
2019-02-26 18:12:40 +0200

    Added primary validation on the user side.

--- js/custom/main.js
+++ js/custom/main.js
@@ -8,6 +8,48 @@ $(document).ready(function () {
     });
     //Init Select2 plugin.
     $('.gender').select2({
-        minimumResultsForSearch: Infinity
+        minimumResultsForSearch: Infinity,
+        placeholder: 'Gender'
+    });
+    //Init Jquery Validate plugin
+    $("#form").validate({
+        rules: {
+            firstName: {
+                required: true,
+                minlength: 2
+            },
+            lastName: {
+                required: true,
+                minlength: 3
+            },
+            password: {
+                required: true,
+                minlength:8
+            },
+            checkbox: {
+                required: true,
+            },
+            email:{
+                required: true,
+            },
+            gender:{
+                required: true,
+            }
+        },
+        messages:{
+            firstName: {
+                required: 'Please enter your first name',
+                minlength: 'At least one character'
+            },
+            lastName: {
+                required: 'Please enter your last name',
+                minlength: 'At least two character'
+            },
+            password: {
+                required: 'Please enter a password',
+                minlength: 'Password can not be less than 8 characters'
+            }
+
+        }
     });
 });
