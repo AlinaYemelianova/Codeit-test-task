7d3ab53a134fcd40c542efd034123324ac3fa0fa
Alina <alinayemelianova8888@gmail.com>
2019-02-26 15:54:08 +0200

    Сreated markup for the form.

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,6 +1,13 @@
+'use strict';
+
 $(document).ready(function () {
+    // Init iCheck plugin.
     $('.i-checks').iCheck({
         checkboxClass: 'icheckbox_square-green',
         radioClass: 'iradio_square-green',
+    });
+    //Init Select2 plugin.
+    $('.gender').select2({
+        minimumResultsForSearch: Infinity
     });
 });
